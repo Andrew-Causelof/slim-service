b83b41e67c0a1b3d9952d0e04156fedde3fe0d99
Andrey <kan@seo-gravity.ru>
2025-03-17 14:58:39 +0300

    init commit

---
+++ api/src/controllers/XLSExporter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Api\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class XLSExporter
+{
+    public static function exportXls(Request $request, Response $response, array $args) {}
+}
